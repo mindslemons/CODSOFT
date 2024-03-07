e4475862c9179c744cf15f2f3530e1e4bc435813
mindslemons <162565137+mindslemons@users.noreply.github.com>
2024-03-07 13:53:10 +0300

    Add files via upload

---
+++ NumberGame/NumbersGame.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+public class NumbersGame {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(100)+ 1;
+        System.out.println("Random number is " + randomNumber);
+        while(true){
+        System.out.println("Enter your  guessing number(1-100):");
+        Scanner scanner = new Scanner(System.in);
+        int playerNo = scanner.nextInt();
+        
+        if (playerNo == randomNumber){
+            System.out.println("You guessed right!");
+            break;
+        }
+        else if(randomNumber > playerNo){
+        System.out.println("The number is higher. Try again");
+        }
+        else{
+            System.out.println("The number is lower. Try again");
+        }
+    }
+}
+}
